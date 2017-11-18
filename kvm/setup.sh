6228c9a70f1d6c43f226b6d5ce40ea44687d94f4
Grégoire Delattre <gregoire.delattre@gmail.com>
2017-11-19 00:25:16 +0100

    Add host networking

--- kvm/setup.sh
+++ kvm/setup.sh
@@ -1,74 +1,78 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# Default values
-RELEASE="stable"
-BASE_URL="http://downloads.overthebox.ovh/${RELEASE}/x86/64"
-IMG_NAME="openwrt-x86-64-combined-squashfs.img.gz"
-IMG_URL="${BASE_URL}/${IMG_NAME}"
+RELEASE="develop"
+IMG_URL="http://downloads.overthebox.net/$RELEASE/targets/x86/64/latest.img.gz"
 DISK_IMG="otb.img"
 IF_NAME="otb0"
 UP_SCRIPT=$(pwd)/qemu-ifup
 DOWN_SCRIPT=$(pwd)/qemu-ifdown
-DEFAULT_IMG_NAME="openwrt-x86-64-combined-squashfs.img.gz"
+DEFAULT_IMG_NAME="latest.img.gz"
 DISK_IMG="otb.img"
+DOCKER_NETWORK="true"
 
 # Enforce root user
 if [ "$(id -u)" -ne 0 ]; then
-    echo "This program must be run as root"
-    exit 1
+	echo "This program must be run as root"
+	exit 1
 fi
 
 usage() {
-    echo -e "Usage:"
-    echo -e "    --image|-i <image_path> Use this image to create the disk"
-    echo -e "    --disk|-d <disk_path>   Use this disk image to create the kvm"
+	echo "Usage:"
+	echo "  --image|-i <image_path> Use this image to create the disk"
+	echo "  --disk|-d <disk_path>   Use this disk image to create the kvm"
+	echo "  --host-network|-n       Use the host network instead of docker"
 }
 
 # Get arguments
-IMG_NAME=""
-while [ "$1" != "" ]; do
-    case $1 in
-        -i | --image )
-            shift
-            IMG_NAME=$1
-            shift
-            ;;
-        -d | --disk )
-            shift
-            DISK_IMG=$1
-            shift
-            ;;
-        -h | --help )
-            usage
-            exit 0
-            ;;
-    esac
-    shift
+IMG_NAME=
+while [ -n "$1" ]; do
+	case $1 in
+		-i | --image )
+			IMG_NAME=$2
+			shift
+			;;
+		-d | --disk )
+			DISK_IMG=$2
+			shift
+			;;
+		-n | --host-network )
+			DOCKER_NETWORK="false"
+			;;
+		-h | --help )
+			usage
+			exit 0
+			;;
+	esac
+	shift
 done
 
 if [ -z "$IMG_NAME" ]; then
-    if [ ! -f "$DEFAULT_IMG_NAME" ]; then
-        echo "Downloading base image"
-        wget "$IMG_URL"
-    fi
-    IMG_NAME=$DEFAULT_IMG_NAME
+	if [ ! -f "$DEFAULT_IMG_NAME" ]; then
+		echo "Downloading base image"
+		wget "$IMG_URL"
+	fi
+	IMG_NAME=$DEFAULT_IMG_NAME
 else
-    if [ ! -f "$IMG_NAME" ]; then
-        echo "File $IMG_NAME not found"
-        exit 1
-    fi
+	if [ ! -f "$IMG_NAME" ]; then
+		echo "File $IMG_NAME not found"
+		exit 1
+	fi
 fi
 
 if [ ! -f "$DISK_IMG" ]; then
-    echo "Extracting base image"
-    gunzip -c "$IMG_NAME" > "$DISK_IMG"
+	echo "Extracting base image"
+	gunzip -c "$IMG_NAME" > "$DISK_IMG"
 fi
 
-kvm \
-    -M q35 \
-    -m size=1024M \
-    -smp cpus=1,cores=1,threads=1 \
-    -drive file="${DISK_IMG}",id=d0,if=none,bus=0,unit=0 -device ide-hd,drive=d0,bus=ide.0 \
-    -net nic \
-    -net tap,ifname="${IF_NAME}",script="${UP_SCRIPT}",downscript="${DOWN_SCRIPT}" \
-    -nographic
+NET_PARAMS="-net nic -net tap,ifname=$IF_NAME,script=$UP_SCRIPT,downscript=$DOWN_SCRIPT"
+[ "$DOCKER_NETWORK" = "false" ] && NET_PARAMS="-net nic -net tap,ifname=qemu0,script=no,downscript=no"
+
+# shellcheck disable=2086
+qemu-system-x86_64 \
+	-enable-kvm \
+	-M q35 \
+	-m size=1024M \
+	-smp cpus=1,cores=1,threads=1 \
+	-drive format=raw,file="$DISK_IMG",id=d0,if=none,bus=0,unit=0 -device ide-hd,drive=d0,bus=ide.0 \
+	$NET_PARAMS \
+	-nographic
