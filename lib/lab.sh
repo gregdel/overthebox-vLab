3f21b15038df481b681987c246c16ac442dff79f
Grégoire Delattre <gregoire.delattre@gmail.com>
2019-03-11 17:14:44 +0100

    Add profiles

--- lib/lab.sh
+++ lib/lab.sh
@@ -1,19 +1,12 @@
 # vim: set ft=sh noexpandtab tabstop=4 shiftwidth=4 softtabstop=4 :
 
+IMG_DIR=$(pwd)/images
+PROFILE_DIR=$(pwd)/profile.d
+CURRENT_PROFILE="$PROFILE_DIR/current"
 BRIDGE_NAME="vlab"
-
-_usage() {
-	echo "Usage:"
-	echo "    --modem-create|-mc <number> <wan_ip> <lan_ip> [script] Creates a modem"
-	echo "    --modem-delete|-md <number>                            Deletes a modem"
-	echo "    --modem-enter|-me <number>                             Enters in the network namespace of the modem"
-	echo "    --client|-c                                      Starts a temporary client"
-	echo "    --cleanup|-d                                     Cleanup the lab"
-	echo "    --dnat|-r <modem> <local_ip> <authorized_ip>     Setup dnat"
-	echo "    --traffic|-t <modem> on|off                      Enable or disable the traffic on a modem"
-	echo "    --netem|-n <modem> <rate> <latency>              Set the latency and rate of the modem"
-	exit 1
-}
+QEMU_UP_SCRIPT=$(pwd)/qemu.hooks.d/ifup
+QEMU_DOWN_SCRIPT=$(pwd)/qemu.hooks.d/ifdown
+QEMU_IMAGE="$IMG_DIR/otb.img"
 
 _log_info() {
 	printf "$(tput setaf 5)-->$(tput setaf 2) %s$(tput setaf 7)\n" "$@"
@@ -36,65 +29,248 @@ _setup_sysctl() {
 	sysctl -p "$sysctl_conf"
 }
 
-_create_modem() {
-	modem_number=$1
-	wan_ip=$2
-	lan_ip=$3
-	script=${4:-default}
-	[ "$modem_number" ] || usage
-	[ "$wan_ip" ] || usage
-	[ "$lan_ip" ] || usage
+_modem_wan_up() {
+	modem_name=$1
+	# shellcheck source=/dev/null
+	.  "$CURRENT_PROFILE/$modem_name"
 
-	modem_name="modem$modem_number"
+	[ "$MODEM_PUBLIC_IP" ] || _log_err "Missing modem public_ip in profile:$profile file:$filename"
+	[ "$MODEM_PRIVATE_IP" ] || _log_err "Missing modem private_ip in profile:$profile file:$filename"
+
+	script=${MODEM_NETWORK_SCRIPT:-default}
 
 	netns_path="/var/run/netns/$modem_name"
 	[ -f "$netns_path" ] && _log_error "$modem_name already exists"
 
 	_setup_sysctl
-	_create_lan_bridge
 
 	_log_info "Creating namespace for $modem_name"
 	ip netns add "$modem_name"
 
-	_log_info "Adding $wan_ip in container $modem_name using the $script script"
+	_log_info "Adding $MODEM_PUBLIC_IP in container $modem_name using the $script script"
 
 	# Configure the WAN
-	"./modem.wan.d/$script" "$modem_name" "$wan_ip"
-
-	# Configure the LAN
-	_log_info "Creating veth interface for the LAN"
-	pseudo_random_name=$(date +%N)
-	lan_name="${modem_name}_lan"
-	ip link add "$lan_name" type veth peer name "$pseudo_random_name"
-	ip link set "$lan_name" master "$BRIDGE_NAME" up
-	ip link set "$pseudo_random_name" netns "$modem_name"
-	ip -n "$modem_name" link set "$pseudo_random_name" name lan
-	ip -n "$modem_name" link set lo up
-	ip -n "$modem_name" link set lan up
-	ip -n "$modem_name" addr add "$lan_ip" dev lan
-	ip netns exec "$modem_name" iptables -t nat -A POSTROUTING -s "$lan_ip" -o wan -j MASQUERADE
+	"./modem.wan.d/$script" "$modem_name" "$MODEM_PUBLIC_IP"
 }
 
-_delete_modem() {
-	modem_number=$1
-	[ "$modem_number" ] || _usage
-	modem_name="modem$modem_number"
+_modem_lan_up() {
+	ifname=$1
+	modem_name=${ifname%%_lan}
+
+	# shellcheck source=/dev/null
+	.  "$CURRENT_PROFILE/$modem_name"
+
+	ip link set "$ifname" netns "$modem_name"
+	ip -n "$modem_name" link set "$1" name lan
+	ip -n "$modem_name" link set lo up
+	ip -n "$modem_name" link set lan up
+
+	ip -n "$modem_name" addr add "$MODEM_PRIVATE_IP" dev lan
+	ip netns exec "$modem_name" iptables -t nat -A POSTROUTING -s "$MODEM_PRIVATE_IP" -o wan -j MASQUERADE
+}
+
+_modem_down() {
+	modem_name=$1
+	# shellcheck source=/dev/null
+	.  "$CURRENT_PROFILE/$modem_name"
 
 	_log_info "Deleting namespace for $modem_name"
 	ip netns del "$modem_name"
-
-	# TODO delete the bridge if there's nothing in it
 }
 
 _modem_enter() {
-	modem_number=$1
-	[ "$modem_number" ] || _usage
-	modem_name="modem$modem_number"
-	PS1="[modem $modem_number] # " sudo ip netns exec "$modem_name" sh
+	shift
+	modem_name=$1
+	PS1="[$modem_name] # " sudo ip netns exec "$modem_name" sh
 }
 
-_create_lan_bridge() {
-	ip link show "$BRIDGE_NAME" >/dev/null 2>/dev/null && return 0
+_modem_show() {
+	profile=$1
+	filename=$2
+	modem_conf="$PROFILE_DIR/$profile/$filename"
+	[ -f "$modem_conf" ] || _log_err "Modem $filename not found in $profile"
+	echo "* $filename:"
+	cat "$modem_conf"
+}
+
+_lan_up() {
 	ip link add name "$BRIDGE_NAME" type bridge
 	ip link set "$BRIDGE_NAME" up
+	ip link set "$1" master "$BRIDGE_NAME" up
+}
+
+_lan_down() {
+	ip link del "$1"
+	ip link del "$BRIDGE_NAME"
+}
+
+_profile_list() {
+	echo "Profiles:"
+	for profile in "$PROFILE_DIR"/*; do
+		echo "* $(basename "$profile")"
+	done
+}
+
+_profile_show() {
+	shift
+	profile=$1
+	[ "$profile" ] || _log_error "Missing profile name"
+	profile_dir="$PROFILE_DIR/$profile"
+
+	echo "Configuration for profile $profile:"
+	for file in "$profile_dir"/*; do
+		name=$(basename "$file")
+		case "$name" in
+			modem*) _modem_show "$profile" "$name" ;;
+		esac
+	done
+}
+
+_profile_up() {
+	shift
+	[ "$1" ] || _log_error "Missing profile name"
+
+	ln -sf "$PROFILE_DIR/$1" "$CURRENT_PROFILE" || true
+
+	for file in "$CURRENT_PROFILE"/*; do
+		name=$(basename "$file")
+		case "$name" in
+			modem*) _modem_wan_up "$name" ;;
+		esac
+	done
+}
+
+_profile_down() {
+	for file in "$CURRENT_PROFILE"/*; do
+		name=$(basename "$file")
+		case "$name" in
+			modem*) _modem_down "$name" ;;
+		esac
+	done
+	rm "$CURRENT_PROFILE"
+}
+
+_qemu_default_net_params() {
+	ifname=$1
+	echo "-net nic -net tap,ifname=$ifname,script=$QEMU_UP_SCRIPT,downscript=$QEMU_DOWN_SCRIPT"
+}
+
+_qemu_start() {
+	# Default lan
+	net_params=$(_qemu_default_net_params "vlab_lan")
+	for file in "$CURRENT_PROFILE"/*; do
+		name=$(basename "$file")
+		case "$name" in
+			modem*)
+				# shellcheck source=/dev/null
+				.  "$file"
+				ifname="${name}_lan"
+				net_params="$net_params $(_qemu_default_net_params "$ifname")"
+				;;
+		esac
+	done
+
+	# shellcheck disable=2086
+	qemu-system-x86_64 \
+		-enable-kvm \
+		-M q35 \
+		-m size=1024M \
+		-smp cpus=1,cores=1,threads=1 \
+		-drive format=raw,file="$QEMU_IMAGE",id=d0,if=none,bus=0,unit=0 -device ide-hd,drive=d0,bus=ide.0 \
+		$net_params \
+		-nographic
+}
+
+_images_list() {
+	echo "Images: "
+	for file in "$IMG_DIR"/*.img; do
+		[ -f "$file" ] || break
+		name=$(basename "$file")
+		echo "* $name"
+	done
+}
+
+_images_download() {
+	shift
+	case "$1" in
+		http*img.gz)
+			filename="latest.img"
+			url=$1
+			;;
+		develop|stable|unstable|testing)
+			filename="latest-$1.img"
+			url="http://downloads.overthebox.net/$1/targets/x86/64/latest.img.gz"
+			;;
+		*)
+			_log_error "Invalid input $1"
+			;;
+	esac
+
+	_log_info "Downloading $url"
+	curl -Ss "$url" | gunzip -c > "$IMG_DIR/$filename"
+	_log_info "File $filename created"
+}
+
+_images_delete() {
+	shift
+	file="$IMG_DIR/$1"
+	[ -f "$file" ] || _log_error "Invalid image: $1"
+	rm "$file"
+}
+
+_images_use() {
+	shift
+	file="$IMG_DIR/$1"
+	[ -f "$file" ] || _log_error "Invalid image: $1"
+	[ -f "$QEMU_IMAGE" ] && rm "$QEMU_IMAGE"
+	ln -s "$file" "$QEMU_IMAGE"
+}
+
+####################################################################
+#
+# TODO: refactoring
+#
+####################################################################
+
+# Setup a dnat
+setup_dnat() {
+	modem=$1
+	local_ip=$2
+	authorized_ip=$3
+
+	_log_info "Authorizing $authorized_ip to connect to $local_ip via $modem"
+	docker exec "$modem" iptables -t nat -F PREROUTING
+	docker exec "$modem" iptables -t nat -A PREROUTING -s "$authorized_ip"/32 -p tcp -m tcp --dport 22 -j DNAT --to-destination "$local_ip":22
+	docker exec "$modem" iptables -t nat -A PREROUTING -s "$authorized_ip"/32 -p tcp -m tcp --dport 443 -j DNAT --to-destination "$local_ip":443
+	_log_info "DNAT set up"
+
+	public_wan=$(docker exec "$modem" curl -s ifconfig.ovh)
+	_log_info "You can now connect with:"
+	_log_info "ssh root@$public_wan"
+	_log_info "https://$public_wan"
+}
+
+modem_traffic_switch() {
+	modem=$1
+	next_state=$2
+	case $next_state in
+		on)
+			docker exec "$modem" iptables -t filter -P FORWARD ACCEPT
+			_log_info "modem traffic allowed";;
+		off)
+			docker exec "$modem" iptables -t filter -P FORWARD DROP
+			_log_info "modem traffic blocked";;
+		*)
+			_log_error "Only on/off values are allowed";;
+	esac
+}
+
+modem_qos() {
+	modem=$1
+	rate=$2
+	latency=$3
+	_log_info "Setting up $modem with a rate of $rate and a latency of $latency"
+	docker exec "$modem" tc qdisc replace dev eth0 root netem rate "$rate" delay "$latency"
+	docker exec "$modem" tc qdisc replace dev wan root netem rate "$rate"
+	_log_info "QoS setup done"
 }
